882ba50cfe571beecc0a542076d5a40ddd3553a8
ThanaponJaiauea <162958446+ThanaponJaiauea@users.noreply.github.com>
2026-02-20 13:25:00 +0700

    create : images icon

--- src/components/loaderChat/loaderChat.jsx
+++ src/components/loaderChat/loaderChat.jsx
@@ -4,16 +4,16 @@
 import Image from "next/image";
 
 import styles from "./Loader.module.css";
-import {useLanguage} from "@/hooks/LanguageProvider";
-export default function LoaderChat({loadPreview, mode}) {
-  const {t} = useLanguage();
+import { useLanguage } from "@/hooks/LanguageProvider";
+export default function LoaderChat({ loadPreview, mode }) {
+  const { t } = useLanguage();
   return (
     <div className="relative w-[40px] h-[40px] z-0 ">
       <div className={styles.loader}></div>
 
       <div className="absolute inset-0 flex items-center justify-center z-[2000]">
         <Image
-          src="/icon/icon_loader.png"
+          src="../../images/icon/icon_loader.png"
           alt="Icon MJM"
           width={32}
           height={32}
@@ -25,7 +25,8 @@ export default function LoaderChat({loadPreview, mode}) {
               mode === "preview"
                 ? "absolute left-[80px] w-[85px] text-center"
                 : "absolute font-[300] text-[14px] ml-36 w-[200px] text-center"
-            } `}>
+            } `}
+          >
             {mode === "preview" ? t.status.thinking : t.status.generating}
           </p>
         ) : null}
